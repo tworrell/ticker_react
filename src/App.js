1508221f92b0709d64efbd17925ef6e7afd1dfe8
tworrell <tara.worrell@gmail.com>
2020-03-10 20:25:20 -0400

    Created the components for the ticker

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class Ticker  extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  };
 
-export default App;
+  //Lifcycle methods -- Built in
+  componentDidMount () {
+    setInterval(() => {
+      this.setState({
+        count: this.state.count + 1,
+      })
+    }, 1000)
+  }
+
+  shouldComponentUpdate (nextProps, nextState) {
+    //this is where we check a condition if it's true or fa;se
+    //and only allow a re-render if the condition is true
+    // if(nextState.count % 3 === 0) return true;
+    //   else return false;
+    return(nextState.count % 3 === 0); //one-liner
+
+  }
+
+  //custom method
+  reset = () => {
+     this.setState({
+       count: 0
+   });
+ };
+
+  //This is a custom method
+  // increment = () => {
+  //   if(this.state.count === 0){
+  //     this.state.count++;
+  //   };
+  // };
+
+
+
+  render () {
+    return(
+      <div>
+      <p>The Ticker is: {this.state.count} </p>
+      <button onClick={this.reset} type="button">Reset the Ticker</button>
+      </div>
+    );
+  };
+};
+
+
+
+export default Ticker;
